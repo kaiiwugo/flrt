d7f9bc61239b47739c1b1ff5ff07ec088cf7c922
kaiiwugo <kiwugo1@gmail.com>
2025-10-28 18:36:12 -0400

    Initial Commit

---
+++ flrt/flrtApp.swift
@@ -0,0 +1,17 @@
+//
+//  flrtApp.swift
+//  flrt
+//
+//  Created by Kaelin Iwugo on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct flrtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ flrtTests/flrtTests.swift
@@ -0,0 +1,17 @@
+//
+//  flrtTests.swift
+//  flrtTests
+//
+//  Created by Kaelin Iwugo on 10/28/25.
+//
+
+import Testing
+@testable import flrt
+
+struct flrtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
